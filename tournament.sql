71bad32efd567ce32e96ffe22c53486f5a3b4c3b
weehaa <vzg1000@mail.ru>
2017-01-07 22:08:02 +0400

    replaced table with view in SQL file

--- tournament.sql
+++ tournament.sql
@@ -6,14 +6,22 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+DROP DATABASE IF EXISTS tournament;
+
 CREATE DATABASE tournament;
 \c tournament
-CREATE TABLE players (id SERIAL, name VARCHAR(50));
-ALTER TABLE players ADD PRIMARY KEY (id);
-CREATE TABLE matches (winner INTEGER REFERENCES players(id),
-                      loser INTEGER REFERENCES players(id),
-                      PRIMARY KEY (winner, loser));
-CREATE TABLE standings (player_id INTEGER REFERENCES players(id),
-                        wins INTEGER default 0,
-                        matches INTEGER default 0);
-ALTER TABLE matches add column id SERIAL;
+
+CREATE TABLE players (id SERIAL PRIMARY KEY, name VARCHAR(50));
+
+CREATE TABLE matches (id SERIAL PRIMARY KEY,
+                      winner INTEGER REFERENCES players(id),
+                      loser INTEGER REFERENCES players(id)
+                    );
+
+CREATE VIEW v_standings AS
+  SELECT p.id, p.name, count(m.winner) AS wins,
+          count(m1.loser) + count(m.winner) AS matches
+    FROM players p LEFT JOIN matches m ON m.winner = p.id
+    LEFT JOIN matches m1 ON m1.loser = p.id
+  GROUP BY p.id, p.name
+  ORDER BY wins DESC;
